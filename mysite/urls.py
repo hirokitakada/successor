181d8517eff17de975565d90571df60b0d071c92
hirokitakada <hirokitakada0402@gmail.com>
2021-02-12 14:34:49 +0900

    広樹dev2/12

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,9 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
 from django.conf import settings
-import base
+# import base
 from django.contrib.sitemaps.views import sitemap
 from django.conf.urls.static import static
 # ログインしないと入れないようにするためのもの
